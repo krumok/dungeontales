d3949d09e2462e2b9300900fcac98fcfb72c466b
krumok <liogiu@gmail.com>
2026-04-22 21:34:20 +0200

    Create ingresso-1.js

---
+++ js/stanze/ingresso-1.js
@@ -0,0 +1,10 @@
+const dati_ingresso_1 = {
+    titolo: "INGRESSO: 1",
+    descrizione: [
+        "L'ambiente è scarno e sporco. Qualcuno si è accampato qui prima di voi.",
+        "Un forte odore di muffa e stoffa bagnata impregna l'aria.",
+        "Un tintinnio di catene eccheggia in lontananza."
+    ],
+    illuminazione: "Luce fievole (visibilità 6m), proveniente dalle scale",
+    // ... tutte le altre liste qui ...
+};
