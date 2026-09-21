7c83f7e80a15275f56c92f724e0f3ee3ad4c3623
krumok <liogiu@gmail.com>
2026-09-21 22:26:14 +0200

    nuove immagini

--- js/stanze/ingresso-1.js
+++ js/stanze/ingresso-1.js
@@ -11,7 +11,7 @@ const DATI_STANZA = {
     ],
     
     // Immagine nel box descrizione (stesso ordine di "descrizioni"; null = nessuna)
-    immaginiDescrizione: [null, "assets/motivi/muffa.webp", "assets/motivi/catene.webp", "assets/motivi/sangue-fango.webp"],
+    immaginiDescrizione: [null, "assets/motivi/muffa.webp", "assets/motivi/catene.jpg", "assets/motivi/sangue-fango.webp"],
 
     illuminazione: "Luce fievole (visibilità 6m), proveniente dalle scale",
     note: "E' possibile accamparsi qui per recuperare PF, PP, PA e Stress.",
